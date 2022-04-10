d1cb0f36078acd6fcfa2dc59aea8306937631773
unknown <aventura.n@mail.rugit config --global user.name>
2022-04-10 17:39:10 +0300

    lesson-40 Added timer,Registration and TextField

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,21 @@
 import React from "react";
 import "./App.css";
-import Button from "./component/button/Button";
+
+//import Posts from "./component/posts/Posts";
+
 import Header from "./component/header/Header";
-import Description from "./component/description/Description";
-import Posts from "./component/posts/Posts";
-import Clicker from "./component/clicker/Clicker";
-import Timer from "./component/timer/Timer";
+import Registration from "./component/registration/Registration";
 
 const App: React.FC = () => {
   return (
     <div className="App">
       <div className="app-container">
-        {/*   <Timer /> */}
-        {/* <Clicker /> */}
+        <Header />
+        <Registration />
 
-        <Posts />
+        {/*  <Posts /> */}
 
-        {/* <Header title="Make your Blog Online"></Header>
-      <Description></Description>
+        {/* 
       <Button
         color="red"
         text="Learn More"
--- src/component/header/Header.tsx
+++ src/component/header/Header.tsx
@@ -1,11 +1,31 @@
+import React, { useEffect, useState } from "react";
+import Timer from "../timer/Timer";
+
 import "./Header.scss";
+import { ReactComponent as LogoIcon } from "../../assets/logo.svg";
 
-type PropsType = {
-  title: string;
-};
+const Header: React.FC = () => {
+  return (
+    <nav className="header-container">
+      <div className="logo">
+        {/* <LogoIcon /> */}
+        <div className="app-name">Blog Online</div>
+      </div>
 
-const Header = ({ title = "Hello, world" }: PropsType) => {
-  return <h1 className="title_style">{title}</h1>;
+      <ul className="links">
+        <li>
+          <a href="/">Posts</a>
+        </li>
+        <li>
+          <a href="/">My posts</a>
+        </li>
+      </ul>
+
+      <div>
+        <Timer />
+      </div>
+    </nav>
+  );
 };
 
 export default Header;
